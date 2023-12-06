4d82f13681e51548049107941b231022464c0120
dilerr <jdilernia99@gmail.com>
2023-12-06 15:14:28 -0300

    aaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import { motion } from 'framer-motion'
-import banner from './assets/ALTORANCHO.png'
+import NoSleep from 'nosleep.js';
 import Loading from './Loading'
 
 function App() {
@@ -41,15 +40,38 @@ function App() {
   const handleKeyDown = (e) => {
     if (e.key === 'Enter' && filteredProducts.length === 1) {
       setSearch('')
+      setFilteredProducts(products)
+
     }
   }
 
+
+  var noSleep = new NoSleep();
+
+  function enableNoSleep() {
+    noSleep.enable();
+    document.removeEventListener('touchstart', enableNoSleep, false);
+  }
+  
+  // Habilitar el seguro para no bloquear la pantalla
+  document.addEventListener('touchstart', enableNoSleep, false)
+
+  
+
+  
+  function reloaderror() {
+    setTimeout(function () {
+      window.location.reload();
+    }, 3000);
+  }
+
+
+
   return (
     <>
+
       <div className="App">
-        <div className="banner">
-          <img src={banner} alt='banner' />
-        </div>
+        
         <div className="search">
           <input
             type="text"
@@ -58,37 +80,53 @@ function App() {
             onChange={handleSearch}
             onKeyDown={handleKeyDown}
           />
+
+          <p>Espere unos segundos.</p>
+
         </div>
         <div className="products-contenedor">
           {filteredProducts.length === 0 && <Loading />}
           {filteredProducts.map((product, key) => {
             
             return (
-              <motion.div className="product-card" key={key}>
+              <div className="product-card" key={key}>
                 <div className='product-header'>
-                  <h2>{product.Modelo}</h2>
+                {product.Descuento === 'SI' ? <div className='porcentaje'><p>{product.Porcentaje}</p></div> : null}
                   <h5>{product.Description}</h5>
-                  <div className='medidas-cont'>
+                  </div>
+                  <div className='product-body'>
+     
+                  <div className='cont-precios'>
+                  {product.Descuento === 'SI' ? <p className='Precio-lista-tachado'>ANTES <span>{product.Precio}</span></p> : <p className='Precio-lista'>Precio Lista: <span>{product.Precio}</span></p> }
+                  {product.Descuento === 'SI' ? <p className='Precio-descuento'>SALE 🔥: <span>{product.Pdesceunto}</span></p> : null}
+                  </div>
+                  <p className='Precio-efectivo'>EFECTIVO: <span>{product.Efecivo}</span></p>
+                </div>
+
+                <div className='product-footer-medidas'>
+                <div className='medidas-cont'>
                     <p className='medidas'>ALTO CM: <span>{product.Alto}</span></p>
                     <p className='medidas'>ANCHO CM: <span>{product.Ancho}</span></p>
                     <p className='medidas'>PROFUNDIDAD CM: <span>{product.Porfundidad}</span></p>
                     <p className='medidas'>DIAMETRO CM: <span>{product.Diametro}</span></p>
                     <p className='medidas'>PESO KG: <span>{product.Peso}</span></p>
                   </div>
-                  <p className='Precio-lista'>Precio Lista: <span>{product.Promocion}</span></p>
-                  <p className='Precio-efectivo'>Precio Efectivo: <span>{product.Efecivo}</span></p>
-                  <p className='info'>3 cuotas sin interes - 15% off abonando en efectivo</p>
                 </div>
-                <div className='product-footer'>
+
+                <div className='product-footer-info'>
+                <p className='info'>3 cuotas sin interes - 20% off abonando en efectivo</p>
                   <p>{product.Name}</p>
                 </div>
-              </motion.div >
+              </div >
             )
           }
           )}
         </div>
-        {error && <p className='error'>Producto no encontrado</p>}
+        {error && <p className='error'>Producto no encontrado</p> && reloaderror()}
+        {}
       </div>
+
+
 
 
 
